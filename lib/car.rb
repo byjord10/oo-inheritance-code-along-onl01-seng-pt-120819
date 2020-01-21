ace132ea2189eaba87615c46d498e3b29dda740c
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-21 23:13:44 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,16 +1,16 @@
 require_relative "./vehicle.rb"
 
 class Car
-  attr_accessor :wheel_size, :number
+  attr_accessor :wheel_size, :wheel_number
 
-  def initialize(wheel_size, number)
+  def initialize(wheel_size, wheel_number)
     @wheel_size = "medium"
-    @number = "4"
+    @wheel_number = "4"
   end
 
   def wheel_number(wheel_number)
     @wheel_number = "2"
-  end 
+  end
 
 
 
