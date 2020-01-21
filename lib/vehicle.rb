f6d819ebb4e7a2a338aed9421bd25b067afb73b0
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-21 23:21:56 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,6 +4,14 @@ class Vehicle
   def initialize(wheel_size, wheel_number)
     @wheel_size = "medium"
     @wheel_number = "4"
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    'filling up!'
   end 
 
 end
